b878c70868d143189a88fd253723a1342e0c4df2
Akinfemiwa <possibleolayode5344@gmail.com>
2023-03-01 19:30:57 +0000

    binary heap from array

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -1,26 +1,23 @@
 #include "binary_trees.h"
 
 /**
- * array_to_avl - builds an AVL tree
+ * array_to_heap - builds a Max Binary Heap tree
  *
  * @array: pointer to the first element of the array
  * @size: number of element in the array
  * Return: pointer to the root node of the AVL tree
  */
-avl_t *array_to_avl(int *array, size_t size)
+heap_t *array_to_heap(int *array, size_t size)
 {
-	avl_t *tree;
+	heap_t *tree;
 	size_t i;
 
 	tree = NULL;
 
 	for (i = 0; i < size; i++)
 	{
-		avl_insert(&tree, array[i]);
+		heap_insert(&tree, array[i]);
 	}
 
-	if (i == size)
-		return (tree);
-
-	return (NULL);
+	return (tree);
 }
